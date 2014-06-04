689c9066e43cb56d93856844d071906bc10dfc22
Tolik Shevchenko <anatolii.shevchenko@p-product.com>
2014-06-04 13:09:55 +0300

    Addition to previous commit.

--- Source/CBL_BlobStore.m
+++ Source/CBL_BlobStore.m
@@ -146,12 +146,12 @@
                                                                             error: NULL];
     if (!blob)
         return nil;
-    return [blob my_map: ^(id filename) {
+    return [blob my_map: (id)^(id filename) {
         CBLBlobKey key;
         if ([[self class] getKey: &key forFilename: filename])
-            return [NSData dataWithBytes: &key length: sizeof(key)];
+            return (id)[NSData dataWithBytes: &key length: sizeof(key)];
         else
-            return (NSData *)nil;
+            return (id)nil;
     }];
 }
 
